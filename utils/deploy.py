65e71ccf5c84093f3da3a88f80c88f82b703ab06
vraiti <vraiti@redhat.com>
2026-08-11 10:47:54 -0400

    auto-commit 10:47:54 08/11/2026
    Signed-off-by: vraiti <vraiti@redhat.com>

--- utils/deploy.py
+++ utils/deploy.py
@@ -19,6 +19,7 @@ POLL_INTERVAL = 2
 
 MODEL_MAP = {
     "qwen3-omni": "Qwen/Qwen3-Omni-30B-A3B-Instruct",
+    "minicpm-o": "openbmb/MiniCPM-o-2_6",
     "flux2": "black-forest-labs/FLUX.2-dev",
 }
 
